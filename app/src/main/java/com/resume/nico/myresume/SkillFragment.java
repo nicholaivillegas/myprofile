97ac273bce4ad6983cbe7a93e594baa03c0a70a7
Nico Villegas <john@tagcash.com>
2017-02-01 10:27:59 +0800

    Todo: expand collapse on experience. Add skills

--- app/src/main/java/com/resume/nico/myresume/SkillFragment.java
+++ app/src/main/java/com/resume/nico/myresume/SkillFragment.java
@@ -1,17 +1,23 @@
 package com.resume.nico.myresume;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
@@ -31,22 +37,44 @@ public class SkillFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_skill, container, false);
         mChart = (PieChart) view.findViewById(R.id.pie_chart);
         Description desc = new Description();
-        desc.setText("Pinakapogi");
-//        desc.setTextAlign(Paint.Align.LEFT);
-//        desc.setPosition(200,700);
-        desc.setTextSize(20);
+        desc.setText("");
+//        desc.setTextSize(20);
         mChart.setDescription(desc);
-        List<PieEntry> entries = new ArrayList<>();
-        entries.add(new PieEntry(4f, "Mark", 0));
-        entries.add(new PieEntry(8f, "Oli", 1));
-        entries.add(new PieEntry(6f, "Dione", 2));
-        entries.add(new PieEntry(12f, "Jovi", 3));
-        entries.add(new PieEntry(50f, "Nico", 4));
-        PieDataSet dataset = new PieDataSet(entries, "# Poginess");
+
+        final List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(10f, "Mobile Development", 0));
+        entries.add(new PieEntry(10f, "Web Development", 1));
+        entries.add(new PieEntry(10f, "System Analysis", 2));
+        entries.add(new PieEntry(10f, "UI/UX Design", 3));
+        PieDataSet dataset = new PieDataSet(entries, "");
         dataset.setColors(ColorTemplate.PASTEL_COLORS);
+        dataset.setHighlightEnabled(true);
+        Legend l = mChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.CENTER);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(true);
         PieData data = new PieData(dataset);
+        data.setValueTextColor(Color.WHITE);
+        data.setValueTextSize(0);
         mChart.setData(data);
-        mChart.animateY(3000);
+        mChart.animateXY(1000, 1000);
+        mChart.setEntryLabelTextSize(10);
+//        mChart.setUsePercentValues(true);
+        mChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                String index = e.getData().toString();
+                String label = entries.get(Integer.parseInt(index)).getLabel();
+                Toast.makeText(getContext(), label, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
         return view;
     }
 
